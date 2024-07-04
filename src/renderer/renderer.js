081fc60a30e8965b2464a59db31e61e982434fe4
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-07-04 12:01:38 -0300

    feat: mp3 convert

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -3,7 +3,8 @@ const ytdl = require('ytdl-core')
 const fs = require('fs')
 const path = require('path')
 const { tmpdir } = require("os")
-const ffmpeg = require('ffmpeg')
+// const ffmpeg = require('ffmpeg')
+const ffmpeg = require('fluent-ffmpeg')
 const { randomUUID } = require("crypto")
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -34,24 +35,37 @@ window.addEventListener("DOMContentLoaded", () => {
         const videoPath = path.resolve(tmpdir(), `${randomUUID()}.mp4`)
         const stream = fs.createWriteStream(videoPath)
 
-        const donwloader = ytdl(info.videoDetails.video_url)
+        const donwloader = ytdl(info.videoDetails.video_url, { quality: "highestaudio" })
 
         donwloader.pipe(stream).on("finish", () => {
-            stream.close()
-            const process = new ffmpeg(videoPath)
+            // const process = new ffmpeg(videoPath)
 
-            process.then(video => {
-                const savePath = path.resolve(path_to_save, `${formatTitle(info.videoDetails.title)}.mp3`)
-                video.fnExtractSoundToMP3(path.resolve(tmpdir(), "audio"), (error, file) => {
-                    if (error) {
-                        console.error(error)
-                    }
+            // process.then(video => {
+            const savePath = path.resolve(path_to_save, `${formatTitle(info.videoDetails.title)}.mp3`)
+            //     video.fnExtractSoundToMP3(path.resolve(tmpdir(), "audio"), (error, file) => {
+            //         if (error) {
+            //             console.error(error)
+            //         }
 
-                    if (file) {
-                        fs.renameSync(file, savePath)
-                    }
+            //         if (file) {
+            //             fs.renameSync(file, savePath)
+            //         }
+            //     })
+            // })
+
+            const stream = fs.createWriteStream(savePath)
+
+            ffmpeg()
+                .input(videoPath)
+                .audioCodec('libmp3lame') // Configura o codec de áudio para MP3
+                .toFormat('mp3') // Define o formato de saída para MP3
+                .on('end', () => {
+                    console.log('Conversão concluída com sucesso!')
                 })
-            })
+                .on('error', (err) => {
+                    console.error('Erro durante a conversão:', err)
+                })
+                .writeToStream(stream, { end: true })
         })
     }
 
